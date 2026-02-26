5b67088ff167312decbf965b4c3fa61f2e17ad98
Olawale <eyakubu442@gmail.com>
2026-02-26 17:35:25 +0100

    search filtering, drawer & carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import ViewAllProductsButton from "@/components/view-all-products-button";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 
 const Homepage = async () => {
+  const featuredProducts = await getFeaturedProducts();
   const  latestProducts = await getLatestProducts();
+
+  
   return (
-    <div className='space-y-8'>
-      <h2 className='h2-bold'>Latest Products</h2>
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList title='Newest Arrivals' data={latestProducts} />
-    </div>
+      <ViewAllProductsButton />
+    </>
   );
 }
  
